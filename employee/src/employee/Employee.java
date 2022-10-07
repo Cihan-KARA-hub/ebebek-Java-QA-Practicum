dcaf9b102fa05cc8bfa74416f9e0fa0d9395b215
Cihan Kara <73289826+Cihan-KARA-hub@users.noreply.github.com>
2022-10-07 03:27:53 +0300

    Add files via upload

---
+++ employee/src/employee/Employee.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package employee;
+
+
+public class Employee {
+
+
+ 
+
+    public static void main(String[] args) 
+    {
+   root p = new root();
+       
+   p.root("cihan", 2000, 50, 2000);
+            System.out.println(p.toString());
+    }
+
+}
---
+++ employee/src/employee/root.java
@@ -0,0 +1,95 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package employee;
+
+/**
+ *
+ * @author cihan
+ */
+public class root {
+   public static String name;
+   public static double salary;
+   public static int workHours;
+   public static int hireYear;
+       
+    public  void root(String isim,int maas, int calismaSaati,int yil)
+    {
+    this.name=isim;
+    this.salary=maas;
+    this.workHours=calismaSaati;
+    this.hireYear=yil;
+    
+    }
+      static  double tax()
+    
+    {   
+        if(salary<1000)
+        {
+            return salary;
+        }
+        else
+        {
+            double vergi=salary*(0.03);
+           
+            return vergi ;
+        }
+        
+    }
+    static double  bonus()
+    {
+        int add=0;
+        if(workHours<=40){
+        return 0;
+    }
+        else
+        {
+            for (int i = 41; i <= workHours; i++)
+            {
+                add+=30;
+            }
+            
+            return add;
+        }
+        
+    }
+    static double raiseSalary(){
+        /*vergi ve bonuslarda çalışan maaşını sadece kontrol amaçlı 
+        kulladığım için burda vergi ve mesai ücretini dikkate almadım.*/
+    int year=2021-hireYear;
+    double pay=salary;
+    
+    if(year<10)
+    {
+    pay=pay*(0.05);
+   
+    return pay;
+    }
+    if(10<=year&&year<20)
+    {
+       pay=pay*(0.1);  
+      
+       return pay;
+        
+    }
+    if(20<=year)
+    {
+        pay=pay*(0.15);    
+        
+         return pay;
+    
+    }
+ return 0;
+    }
+   public  String toString()
+    {
+        double total=salary+raiseSalary()+bonus()-tax();
+   return " Calisan ismi: "+name+"\n"+" Calisanin mesai saati: "
+           +workHours+"\n"+" Calisanin ise baslagic yili: "+hireYear+"\n"+" Calisan burut maasi: "+salary+
+           "\n"+" vergi ucreti: "+tax()+"\n"+" mesai ucreti: "+bonus()+"\n" +" yillik zam: "+raiseSalary()+"\n"+"------------------"
+           +"\n"+" total maas: "+total;
+           }
+}
+
+
